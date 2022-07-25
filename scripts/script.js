00003fdbed8fa075972b98cf9068354080f9fa7c
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-07-25 12:52:32 +0200

    js second part finished

--- scripts/script.js
+++ scripts/script.js
@@ -5,25 +5,37 @@ body.setAttribute('onload', 'setEvents()');
 //global variables decralation
 const button = document.querySelectorAll('button');
 const line = document.getElementsByClassName('line');
-let i;
+let winnerChecker = new Array(3); // array for checking winner
+let i, j;
 
-
+//function for setting events
 const setEvents = () => {
     for(i=0; i<button.length; i++) {
         button[i].setAttribute('onclick', 'markSelected_user(this)');
     }
 }
 
+//function for marking user selected square
 const markSelected_user = (element) => {
     element.setAttribute('class', 'selected-user');
-    setTimeout(markSelected_computer, 800);
+    setTimeout(markSelected_device, 800);
 }
 
-const markSelected_computer = () => {
+//function for marking device selected square
+const markSelected_device = () => {
     button[5].setAttribute('class', 'selected-device');
-    setTimeout(checkWinner, 100)
+    setTimeout(checkWinnerHorizontally, 100)
 }
 
-const checkWinner = () => {
-    console.log('winner checked');
+//function for checking winner horizontally
+
+const checkWinnerHorizontally = () => {
+    
+    for(i=0; i<line.length; i++) {
+
+        let square_line = line[i].childNodes;
+        for(j=0; j<square_line.length; i++) {
+            console.log(square_line[j]);
+        }
+    }
 }
